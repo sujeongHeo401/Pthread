8ca4edb87c247e0d113268ff99f49791c634d048
sujeongHeo401 <86057499+sujeongHeo401@users.noreply.github.com>
2022-01-15 16:09:19 +0900

    Create pthreadsimple3.c

---
+++ pthreadsimple3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *t_function(void *ptr){
+    char *message;
+    message = (char *) ptr;
+    printf("%s start  \n", message); // 출력
+    sleep(5);
+    printf("%s end  \n", message); // 출력
+    pthread_exit((void *)100);
+    return 0;
+}
+
+int main(){
+    pthread_t thread1, thread2;
+    const char *message1 = "Thread1";
+    const char *message2 = "Thread2";
+    int ret, status;
+
+    ret = pthread_create(&thread1, NULL, t_function, (void*)message1);
+    if(ret < 0){
+        perror("thread create error");
+    }
+    pthread_join(thread1, (void **)&status);
+    printf("thread1 returns: %d\n", status);
+
+    ret = pthread_create(&thread2, NULL, t_function, (void*)message2);
+    if (ret < 0){
+        perror("thread create error");
+    }
+    //pthread_detach(thread2);
+    sleep(10);
+    return 0;
+}
